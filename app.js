15d937d898d3007f7e6092dfa70b2a07e125fd02
byio <ben@benyang.io>
2018-12-21 22:04:58 +0800

    set up middleware to handle Not Found error and catchall for all other errors

--- app.js
+++ app.js
@@ -19,18 +19,33 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // allow CORS (before routes middleware)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   if (req.methods === 'OPTIONS') {
     res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, DELETE"
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE'
     );
     return res.status(200).json({});
   }
+  next();
+});
+
+// general error handling
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
   next();
 });
 
