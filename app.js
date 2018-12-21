5096f6fe5072d250cfb0ddbfd4edeb9d59dd01e1
byio <ben@benyang.io>
2018-12-21 21:58:39 +0800

    set up middleware to handle CORS

--- app.js
+++ app.js
@@ -17,4 +17,21 @@ app.use(
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+// allow CORS (before routes middleware)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.methods === 'OPTIONS') {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
 module.exports = app;
