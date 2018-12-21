91d1bf7486b5ed7ccd28e294aa8b86cc847d3a40
byio <ben@benyang.io>
2018-12-21 18:03:29 +0800

    configure and use morgan logger middleware; minor amendment in package.json

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// morgan logger middleware
+app.use(
+  morgan(
+    app.get('env') === 'production'
+      ? 'combined'
+      : 'dev'
+  )
+);
 
 module.exports = app;
