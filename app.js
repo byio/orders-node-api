36e0e9bf3f17e9b6a9eac12bb7ae3007abd26acb
byio <ben@benyang.io>
2018-12-21 21:08:30 +0800

    configure bodyParser middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,5 +12,9 @@ app.use(
       : 'dev'
   )
 );
+
+// body parser middleware
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = app;
