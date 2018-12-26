1af926a3dd9784bc8dd77477fca79ee74dbcceec
byio <ben@benyang.io>
2018-12-26 19:26:00 +0800

    create user_get_all controller function

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,3 +1,12 @@
-exports.user_get_all = (req, res, next) => {
+const db = require('../db');
 
+exports.user_get_all = (req, res) => {
+  db.query(
+    'SELECT * FROM users',
+    null,
+    (err, result) => {
+      if (err) res.status(500).json(err);
+      res.status(200).json(result.rows);
+    }
+  )
 };
